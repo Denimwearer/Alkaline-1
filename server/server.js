40cb60aff525c2e962faad5ad3fd0877ebce29b4
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2023-01-04 21:43:15 -0500

    basic server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Server is running on port:" + PORT);
+});
